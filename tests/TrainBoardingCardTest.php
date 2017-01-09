a63010162a5c9a1ed9da0498b03a3fc25ec9ca56
Koray <koraykupe@gmail.com>
2017-01-09 13:33:14 +0300

    Fixed typo

--- tests/TrainBoardingCardTest.php
+++ tests/TrainBoardingCardTest.php
@@ -7,7 +7,7 @@ class TrainBoardingCardTest extends TestCase
 {
     public function testReturnText()
     {
-        $flightBoardingCard = new \App\TripSorter\BoardingCards\TrainBoardingCard('Madrid', 'Barselona', '45B', '78A');
+        $flightBoardingCard = new \App\TripSorter\BoardingCards\TrainBoardingCard('Madrid', 'Barcelona', '45B', '78A');
         $this->assertEquals($flightBoardingCard->toString(), "Take train 78A from Madrid to Barcelona. Sit in seat 45B.");
     }
 }
