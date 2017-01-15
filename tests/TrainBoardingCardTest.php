5fa587dafbdc31838d2ccf876d3c1acd5c70e996
Koray <koraykupe@gmail.com>
2017-01-15 12:30:30 +0200

    Update TrainBoardingCardTest.php

--- tests/TrainBoardingCardTest.php
+++ tests/TrainBoardingCardTest.php
@@ -5,7 +5,7 @@ use Laravel\Lumen\Testing\DatabaseTransactions;
 
 class TrainBoardingCardTest extends TestCase
 {
-    public function testReturnText()
+    public function test_return_text()
     {
         $flightBoardingCard = new \App\TripSorter\BoardingCards\TrainBoardingCard('Madrid', 'Barcelona', '45B', '78A');
         $this->assertEquals($flightBoardingCard->toString(), "Take train 78A from Madrid to Barcelona. Sit in seat 45B.");
